a6057bb23e42d8cb2d87d9d80923f38b7e102e5b
dudosyka <45633581+dudosyka@users.noreply.github.com>
2022-04-08 23:41:44 +0300

    Friends table structure

---
+++ src/user/models/FriendModels.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS `FriendModels` (
+  `child` int(11) NOT NULL,
+  `parent` int(11) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16;
+
+ALTER TABLE `FriendModels`
+  ADD PRIMARY KEY (`child`,`parent`) USING BTREE,
+  ADD KEY `parent` (`parent`);
+
+ALTER TABLE `FriendModels`
+  ADD CONSTRAINT `FriendModels_ibfk_1` FOREIGN KEY (`child`) REFERENCES `UserModels` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FriendModels_ibfk_2` FOREIGN KEY (`parent`) REFERENCES `UserModels` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
